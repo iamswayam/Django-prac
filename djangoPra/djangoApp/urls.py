53799841f06bf09815098ddf04d29322c828d75b
DevSwayam <swayamsiddhap@gmail.com>
2021-10-10 13:43:31 +0530

    Revert "navbar.html"
    This reverts commit a9f97c7ab6a29dcc58548f9d76509ba8dd1d4f1c.

--- djangoPra/djangoApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import TestView, HomePage
-
-urlpatterns = [
-    path('', HomePage, name='HomePage'),
-    path('test/', TestView.as_view(), name='TestView'),
-]
